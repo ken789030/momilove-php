542fbfb13bd9ffbe2a511f5f5682fb878655577c
ken <ken789030@gmail.com>
2023-01-02 14:15:31 +0800

    調整訂單金額超額

--- app/Validate/OrderOverflowValidator.php
+++ app/Validate/OrderOverflowValidator.php
@@ -6,7 +6,8 @@ class OrderOverflowValidator implements OrderVaildatorInterface
 
     public function validator(Order $order)
     {
-        if ($this->order->amount->isOverflow())
+        $overflowAmount = 5000;
+        if ($this->order->amount > $overflowAmount)
         {
             throw new Exception('The Order amount is overflow.');
         }
